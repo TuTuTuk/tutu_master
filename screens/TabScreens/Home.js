f61ce1ab698dea3cd8200a4cf9dbeb0a4307d920
Soo <lider28@naver.com>
2023-05-11 20:26:06 +0900

    feat : 알림창 생성

--- screens/TabScreens/Home.js
+++ screens/TabScreens/Home.js
@@ -166,9 +166,24 @@ const PopularBoardBox = styled.View`
 const Home =({navigation:{navigate}})=>{
     const [click,setClick] = useState(false);
     const [modalVisible,setModalVisible] = useState(false)
-
+    const [alarmVisible, setAlarmVisible] = useState(true)
+    const oPress =  () => {
+        setAlarmVisible(!alarmVisible)
+    }
     return(
         <Container>
+            <View style={{position:"absolute", display: alarmVisible ? "flex" : "none", width:"100%", height:"100%", backgroundColor:"rgba(1,1,1,0.5)", zIndex:10, alignItems:"center"}}>
+                <View style={{alignItems:"center", backgroundColor:"white", width:"90%",height:"40%", position:"absolute", marginTop:"13%", zIndex:20, borderRadius:10 }}>
+                    <Text style={{margin:16, fontSize:16, fontWeight:"700"}}>알림</Text>
+                    <View style={{ width:"100%", height:40, flexDirection:"row", alignItems:"center",}}>
+                        <View style={{width:10, height:10, backgroundColor:"#F5B909", borderRadius:5, marginLeft:15,marginRight:10}}></View>
+                        <View style={{flexDirection:"row",  alignItems:"center", width:"85%", justifyContent:"space-between"}}>
+                            <Text style={{fontSize:14, fontWeight:"400"}}>[포인트] 댓글/덧글 작성으로 +30P</Text>
+                            <Text style={{fontSize:8, fontWeight:"400"}}>1,930 P</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
             <ModalTwoOptions 
                 visible={modalVisible} 
                 setvisible={setModalVisible}
@@ -188,7 +203,7 @@ const Home =({navigation:{navigate}})=>{
                 </LoginBtn>
                 <ConfigureAlarm>
                     {auth().currentUser ? 
-                        <AlarmButton>
+                        <AlarmButton onPress={()=>oPress()}>
                             <AlarmImage
                                 resizeMode="stretch"
                                 source={require('../../images/alarm.png')}
