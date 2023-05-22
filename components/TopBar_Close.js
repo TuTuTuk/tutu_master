917c5d896e0d8e47202ef4a49b1dbc4b73f45c19
Soo <lider28@naver.com>
2023-05-23 00:46:22 +0900

    refactor : 선후배 매칭 코드 수정

--- components/TopBar_Close.js
+++ components/TopBar_Close.js
@@ -17,17 +17,6 @@ const Container = styled.View`
 const PressableBox = styled.Pressable`
     width: 10%;
 `;
-//--------------------------------
-const BackIcon = styled.Image`
-    width:100%;
-    height: 100%;
-`;
-const PlusIcon = styled.Image`
-    width:5%;
-    height: 100%;
-`;
-//--------------------------------
-
 
 const TitleText = styled.Text`
     font-weight: 600;
@@ -42,12 +31,10 @@ const TopBar_Close=()=>{
             <PressableBox
                 onPressOut={()=>navigation.goBack()}
             >
-                <BackIcon style={{marginLeft:20}} source={require('../images/Back.png')}/>
+                <Image style={{marginLeft:20}} source={require('../images/Back.png')}/>
             </PressableBox>
             <TitleText>선-후배 매칭</TitleText>
-            <View>
-                <Image style={{marginRight:20}} source={require('../images/Close.png')}/>
-            </View>
+            <Image style={{marginRight:20}} source={require('../images/Close.png')}/>
         </Container>
     )
 }
