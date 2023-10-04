ce128b0fdff6d94e50d881328f1b0b7162a4376e
minjeong <minjeong9919@naver.com>
2023-10-04 13:53:25 +0900

    TabIcon change

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { Image, StyleSheet, Text} from 'react-native';
+import { Image, StyleSheet, Text } from 'react-native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/TabScreens/Home";
 import Board_min from "../screens/TabScreens/Board_min";
@@ -11,25 +11,9 @@ import LinearGradient from 'react-native-linear-gradient';
 import styled from "styled-components/native";
 import Icon from "react-native-vector-icons/Ionicons";
 import MakeTimeTable from "../screens/StackScreens/Timetable/Timetable";
+ 
 
 const Tab = createBottomTabNavigator();
-
-const getTabIconName = (routeName) => {
-    switch (routeName) {
-        case 'Home':
-            return 'home-outline'
-        case 'Timetable':
-            return 'time-outline'
-        case 'Board_min':
-            return 'clipboard-outline'
-        case 'Matching_min': 
-            return 'people-outline'
-        case 'My':
-            return 'person-outline'
-        default:
-            break;
-    }
-};
 
 const Tabs=()=>(
     <Tab.Navigator 
@@ -47,19 +31,19 @@ const Tabs=()=>(
         })} 
     >
         <Tab.Screen name="Home" component={Home} options={{
-            tabBarIcon:({color,size,focused})=><Icon name={getTabIconName('Home')} color='white' size={size}/>
+            tabBarIcon:({color,size,focused})=><Image source={require('../images/TabIcon/Home.png')} color='white' size={size}/>
         }}/>
         <Tab.Screen name="Timetable" component={Timetable} options={{
-            tabBarIcon:({color,size,focused})=><Icon name={getTabIconName('Timetable')} color='white' size={size}/>
+            tabBarIcon:({color,size,focused})=><Image source={require('../images/TabIcon/Timetable.png')} color='white' size={size}/>
         }}/>
         <Tab.Screen name="Board_min" component={Board_min} options={{
-            tabBarIcon:({color,size,focused})=><Icon name={getTabIconName('Board_min')} color='white' size={size}/>
+            tabBarIcon:({color,size,focused})=><Image source={require('../images/TabIcon/Board.png')} color='white' size={size}/>
         }}/>
         <Tab.Screen name="Matching_min" component={Matching_min} options={{
-            tabBarIcon:({color,size,focused})=><Icon name={getTabIconName('Matching_min')} color='white' size={size}/>
+            tabBarIcon:({color,size,focused})=><Image source={require('../images/TabIcon/Matching.png')} color='white' size={size}/>
         }}/>
         <Tab.Screen name="My" component={My} options={{
-            tabBarIcon:({color,size,focused})=><Icon name={getTabIconName('My')} color='white' size={size}/>
+            tabBarIcon:({color,size,focused})=><Image source={require('../images/TabIcon/My.png')} color='white' size={size}/>
         }}/>
     </Tab.Navigator>
 )
