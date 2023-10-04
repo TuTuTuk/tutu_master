b17d407af37bf5e69fa534a7c2c283d1f720a735
minjeong <minjeong9919@naver.com>
2023-10-04 13:35:03 +0900

    tabBar gradient

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -12,41 +12,54 @@ import styled from "styled-components/native";
 import Icon from "react-native-vector-icons/Ionicons";
 import MakeTimeTable from "../screens/StackScreens/Timetable/Timetable";
 
-const BottomImge = styled.Image`
-    width:30px;
-    height:30px;
-`;
-
 const Tab = createBottomTabNavigator();
 
+const getTabIconName = (routeName) => {
+    switch (routeName) {
+        case 'Home':
+            return 'home-outline'
+        case 'Timetable':
+            return 'time-outline'
+        case 'Board_min':
+            return 'clipboard-outline'
+        case 'Matching_min': 
+            return 'people-outline'
+        case 'My':
+            return 'person-outline'
+        default:
+            break;
+    }
+};
+
 const Tabs=()=>(
-    <Tab.Navigator screenOptions={{
-        headerShown:false,
-        tabBarStyle:{
-            borderTopLeftRadius:30,
-            borderTopRightRadius:30,
-            
-        }
-    }}>
+    <Tab.Navigator 
+        screenOptions={({route})=>({
+            tabBarStyle:{height:56,shadowColor:'transparent'},
+            headerShown:false,
+            tabBarBackground:()=>(
+                <LinearGradient 
+                    colors={['#0062FF', '#0A7DFF', '#1398FF']}
+                    style={{height:56,borderTopLeftRadius:15,borderTopRightRadius:15}}
+                    start={{x:1,y:0}} end={{x:0,y:0}}
+                />
+            ),
+            tabBarLabel:()=>{return null},
+        })} 
+    >
         <Tab.Screen name="Home" component={Home} options={{
-            tabBarIcon:({color,size,focused})=><Icon name="home-outline" color={color} size={size}/>
-            
+            tabBarIcon:({color,size,focused})=><Icon name={getTabIconName('Home')} color='white' size={size}/>
         }}/>
         <Tab.Screen name="Timetable" component={Timetable} options={{
-            tabBarIcon:({color,size,focused})=><Icon name="time-outline" color={color} size={size}/>
-            
+            tabBarIcon:({color,size,focused})=><Icon name={getTabIconName('Timetable')} color='white' size={size}/>
         }}/>
         <Tab.Screen name="Board_min" component={Board_min} options={{
-            tabBarIcon:({color,size,focused})=><Icon name="clipboard-outline" color={color} size={size}/>
-            
+            tabBarIcon:({color,size,focused})=><Icon name={getTabIconName('Board_min')} color='white' size={size}/>
         }}/>
         <Tab.Screen name="Matching_min" component={Matching_min} options={{
-            tabBarIcon:({color,size,focused})=><Icon name="people-outline" color={color} size={size}/>
-            
+            tabBarIcon:({color,size,focused})=><Icon name={getTabIconName('Matching_min')} color='white' size={size}/>
         }}/>
         <Tab.Screen name="My" component={My} options={{
-            tabBarIcon:({color,size,focused})=><Icon name="person-outline" color={color} size={size}/>
-            
+            tabBarIcon:({color,size,focused})=><Icon name={getTabIconName('My')} color='white' size={size}/>
         }}/>
     </Tab.Navigator>
 )
--- screens/StackScreens/Research/HotResearch.js
+++ screens/StackScreens/Research/HotResearch.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from "react";
-import { FlatList,ScrollView,Text } from "react-native";
+import { FlatList,ScrollView,Text,View } from "react-native";
 import styled from "styled-components/native";
 import { useNavigation } from '@react-navigation/native';
 import auth from "@react-native-firebase/auth";
@@ -9,10 +9,6 @@ import LinearGradient from 'react-native-linear-gradient';
 //---------------------------------------------------------------
 //-------------------------인기 키워드----------------------------
 //---------------------------------------------------------------
-
-const AllBox = styled.View`
-    //border: 1px;
-`;
 
 const HotKeywordBox = styled.View`
     //border  : 1px;
@@ -52,7 +48,6 @@ const HotKeywordBox = styled.View`
         `;
     const KeywordTextBox = styled.View`
         height: 30px;
-        //border: 1px;
         justify-content: center;
         width : 80%;
     `;
@@ -77,100 +72,23 @@ const HotKeywordBox = styled.View`
 
 const HotResearch = (HotKwd)=>{
 
-    const HotKWD = HotKwd.HotKwd
+    let HotKWD = HotKwd.HotKwd
+    HotKWD = HotKWD.slice(0,10);
     return(
-        <AllBox>
-            <HotKeywordBox>
-                <HotNumberBox><NumberTopText>1</NumberTopText></HotNumberBox>
-                {
-                HotKWD[0] == null?
+        <View>
+            { HotKWD.map((keyword,index)=>{
+                return(<HotKeywordBox>
+                    <HotNumberBox><NumberTopText>{index+1}</NumberTopText></HotNumberBox>
+                    { keyword == null?
                     <KeywordTextBox><KeywordText></KeywordText></KeywordTextBox>
-                :<KeywordTextBox><KeywordText>{HotKWD[0].id}</KeywordText></KeywordTextBox>
-                }
-                <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
-            </HotKeywordBox>
-            <HotKeywordBox>
-                <HotNumberBox><NumberTopText>2</NumberTopText></HotNumberBox>
-                {
-                    HotKWD[1] == null?
-                    <KeywordTextBox><KeywordText></KeywordText></KeywordTextBox>
-                    :<KeywordTextBox><KeywordText>{HotKWD[1].id}</KeywordText></KeywordTextBox>
-                }
-                <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
-                </HotKeywordBox>
-            <HotKeywordBox>
-                <HotNumberBox><NumberTopText>3</NumberTopText></HotNumberBox>
-                {
-                    HotKWD[2] == null?
-                    <KeywordTextBox><KeywordText></KeywordText></KeywordTextBox>
-                    :<KeywordTextBox><KeywordText>{HotKWD[2].id}</KeywordText></KeywordTextBox>
-                }
-                <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
-            </HotKeywordBox>
-            <HotKeywordBox>
-                <HotNumberBox><NumberText>4</NumberText></HotNumberBox>
-                {
-                    HotKWD[3] == null?
-                    <KeywordTextBox><KeywordText></KeywordText></KeywordTextBox>
-                    :<KeywordTextBox><KeywordText>{HotKWD[3].id}</KeywordText></KeywordTextBox>
-                }
-                        <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
+                    :<KeywordTextBox><KeywordText>{keyword.id}</KeywordText></KeywordTextBox>
+                    }
+                    <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
                     </HotKeywordBox>
-                    <HotKeywordBox>
-                        <HotNumberBox><NumberText>5</NumberText></HotNumberBox>
-                        {
-                            HotKWD[4] == null?
-                            <KeywordTextBox><KeywordText></KeywordText></KeywordTextBox>
-                            :<KeywordTextBox><KeywordText>{HotKWD[4].id}</KeywordText></KeywordTextBox>
-                        }
-                        <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
-                    </HotKeywordBox>
-                    <HotKeywordBox>
-                        <HotNumberBox><NumberText>6</NumberText></HotNumberBox>
-                        {
-                            HotKWD[5] == null?
-                            <KeywordTextBox><KeywordText></KeywordText></KeywordTextBox>
-                            :<KeywordTextBox><KeywordText>{HotKWD[5].id}</KeywordText></KeywordTextBox>
-                        }
-                        <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
-                    </HotKeywordBox>
-                    <HotKeywordBox>
-                        <HotNumberBox><NumberText>7</NumberText></HotNumberBox>
-                        {
-                            HotKWD[6] == null?
-                            <KeywordTextBox><KeywordText></KeywordText></KeywordTextBox>
-                            :<KeywordTextBox><KeywordText>{HotKWD[6].id}</KeywordText></KeywordTextBox>
-                        }
-                        <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
-                    </HotKeywordBox>
-                    <HotKeywordBox>
-                        <HotNumberBox><NumberText>8</NumberText></HotNumberBox>
-                        {
-                            HotKWD[7] == null?
-                            <KeywordTextBox><KeywordText></KeywordText></KeywordTextBox>
-                            :<KeywordTextBox><KeywordText>{HotKWD[7].id}</KeywordText></KeywordTextBox>
-                        }
-                        <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
-                    </HotKeywordBox>
-                    <HotKeywordBox>
-                        <HotNumberBox><NumberText>9</NumberText></HotNumberBox>
-                        {
-                            HotKWD[8] == null?
-                            <KeywordTextBox><KeywordText></KeywordText></KeywordTextBox>
-                            :<KeywordTextBox><KeywordText>{HotKWD[8].id}</KeywordText></KeywordTextBox>
-                        }
-                        <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
-                    </HotKeywordBox>
-                    <HotKeywordBox>
-                        <HotNumberBox><NumberText>10</NumberText></HotNumberBox>
-                        {
-                            HotKWD[9] == null?
-                            <KeywordTextBox><KeywordText></KeywordText></KeywordTextBox>
-                            :<KeywordTextBox><KeywordText>{HotKWD[9].id}</KeywordText></KeywordTextBox>
-                        }
-                        <UpDownBox><UpDownIcon source={require('../../../images/UpArrow.png')}></UpDownIcon></UpDownBox>
-                    </HotKeywordBox>
-                </AllBox>
+                )
+            })}
+            
+        </View>
     )
 }
 export default HotResearch;
